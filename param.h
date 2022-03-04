e2c4a8d596c77f78530a61c6fb26f579cb107265
Austin M. Reppert <austinmreppert@protonmail.com>
2022-03-04 12:39:07 -0500

    Fixes kernel stack is too small to fmt print numbers.

--- param.h
+++ param.h
@@ -1,15 +1,27 @@
-#define NPROC        64  // maximum number of processes
-#define KSTACKSIZE 4096  // size of per-process kernel stack
-#define NCPU          8  // maximum number of CPUs
-#define NOFILE       16  // open files per process
-#define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
-#define NDEV         10  // maximum major device number
-#define ROOTDEV       1  // device number of file system root disk
-#define MAXARG       32  // max exec arguments
-#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
-#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+// maximum number of processes
+#define NPROC             64
+// The size of per-process kernel stack.
+#define KERNEL_STACK_SIZE 16384
+// maximum number of CPUs
+#define NCPU              8
+// open files per process
+#define NOFILE            16
+// open files per system
+#define NFILE             100
+// maximum number of active i-nodes
+#define NINODE            50
+// maximum major device number
+#define NDEV              10
+// device number of file system root disk
+#define ROOTDEV           1
+// max exec arguments
+#define MAXARG            32
+// max # of blocks any FS op writes
+#define MAXOPBLOCKS       10
+// max data blocks in on-disk log
+#define LOGSIZE           (MAXOPBLOCKS*3)
+// size of disk block cache
+#define NBUF              (MAXOPBLOCKS*3)
 #ifdef PDX_XV6
 #define FSSIZE       2000  // size of file system in blocks
 #else
