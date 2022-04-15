a50008b400cd07f71f89f90607b17b61570bcbd0
Austin M. Reppert <austinmreppert@protonmail.com>
2022-04-14 23:26:54 -0400

    Cleanup local interrupt controller.

--- src/local_interrupt_controller.rs
+++ src/local_interrupt_controller.rs
@@ -1,87 +1,98 @@
-use memory_layout::map_physical_virtual;
+//! See chapter 10, "Advanced Programmable Interrupt Controller (APIC)" of the "Intel® 64 and IA-32 Architectures Software Developer’s Manual"
+//! See also, https://wiki.osdev.org/APIC#Local_APIC_and_IO-APIC.
+
 use traps::{IRQ_ERROR, IRQ_SPURIOUS, IRQ_TIMER, T_IRQ0};
 
 pub static mut LOCAL_INTERRUPT_CONTROLLER: *mut u32 = 0 as *mut u32;
 
-// Spurious Interrupt Vector
-const ID: u32 = 0x0020;
-const VERSION: u32 = 0x0030;
-const SVR: u32 = 0x00F0;
-const ENABLE: u32 = 0x00000100;
-const TDCR: u32 = 0x03E0;
-const X1: u32 = 0x0000000B;
-const TIMER: u32 = 0x0320;
-const PERIODIC: u32 = 0x00020000;
-const TICR: u32 = 0x0380;
-const LINT0: u32 = 0x0350;
-const LINT1: u32 = 0x0360;
-const MASKED: u32 = 0x00010000;
-const PCINT: u32 = 0x0340;
-const ERROR: u32 = 0x0370;
-const ESR: u32 = 0x0280;
-const EOI: u32 = 0x00B0;
-const ICRHI: u32 = 0x0310;
-const ICRLO: u32 = 0x0300;
-const DELIVS: u32 = 0x00001000;
-const BCAST: u32 = 0x00080000;
-const TPR: u32 = 0x0080;
-const INIT: u32 = 0x00000500;
-const LEVEL: u32 = 0x00008000;
+// Registers
+// The offsets are divided by 4 because each register is 4 bytes allowing us to index into
+// LOCAL_INTERRUPT_CONTROLLER like an array.
+const ID: u32 = 0x0020 / 4;
+const VERSION: u32 = 0x0030 / 4;
+const SPURIOUS_INTERRUPT_VECTOR: u32 = 0x00F0 / 4;
+const TIMER_INITIAL_COUNT: u32 = 0x0380 / 4;
+const TIMER_DIVIDE_CONFIGURATION: u32 = 0x03E0 / 4;
+const TIMER_LOCAL_VECTOR_TABLE: u32 = 0x0320 / 4;
+const LOCAL_INTERRUPT_0_PIN: u32 = 0x0350 / 4;
+const LOCAL_INTERRUPT_1_PIN: u32 = 0x0360 / 4;
+const PERFORMANCE_MONITORING_COUNTERS_LOCAL_VECTOR_TABLE: u32 = 0x0340 / 4;
+const ERROR_LOCAL_VECTOR_TABLE: u32 = 0x0370 / 4;
+const ERROR_STATUS: u32 = 0x0280 / 4;
+const END_OF_INTERRUPT: u32 = 0x00B0 / 4;
+const INTERRUPT_COMMAND_HIGH: u32 = 0x0310 / 4;
+const INTERRUPT_COMMAND_LOW: u32 = 0x0300 / 4;
+const TASK_PRIORITY: u32 = 0x0080 / 4;
 
+// Constants
+const ENABLE: u32 = 0x00000100;
+// Divide counts by 1.
+const TIMER_DIVIDE_VALUE: u32 = 0x0000000B;
+const PERIODIC: u32 = 0x00020000;
+const MASKED: u32 = 0x00010000;
+const DELIVERY_STATUS: u32 = 0x00001000;
+// Send to all APICs, including self.
+const BROADCAST: u32 = 0x00080000;
+const INIT: u32 = 0x00000500;
+const LEVEL_TRIGGERED: u32 = 0x00008000;
+
+/// Initialize the local interrupt controller.
 pub unsafe fn init() {
-/*  if LOCAL_INTERRUPT_CONTROLLER.is_null() {
+  if LOCAL_INTERRUPT_CONTROLLER.is_null() {
     println!("Local interrupt controller is undefined.");
     return;
-  }*/
-  //LOCAL_INTERRUPT_CONTROLLER = map_physical_virtual(LOCAL_INTERRUPT_CONTROLLER as usize) as *mut u32;
+  }
+
   // Enable local APIC; set spurious interrupt vector.
+  write(SPURIOUS_INTERRUPT_VECTOR, ENABLE | (T_IRQ0 + IRQ_SPURIOUS));
 
-  write(SVR, ENABLE | (T_IRQ0 + IRQ_SPURIOUS));
-
-  // The timer repeatedly counts down at bus frequency
-  // from lapic[TICR] and then issues an interrupt.
-  // If xv6 cared more about precise timekeeping,
-  // TICR would be calibrated using an external time source.
-  write(TDCR, X1);
-  write(TIMER, PERIODIC | (T_IRQ0 + IRQ_TIMER));
-  write(TICR, 1000000);
+  // The timer repeatedly counts down at bus frequency.
+  // from LOCAL_INTERRUPT_CONTROLLER[TIMER_INITIAL_COUNT] and then issues an interrupt.
+  // If xv6 cared more about precise timekeeping, TIMER_INITIAL_COUNT would be calibrated using an external time source.
+  write(TIMER_DIVIDE_CONFIGURATION, TIMER_DIVIDE_VALUE);
+  write(TIMER_LOCAL_VECTOR_TABLE, PERIODIC | (T_IRQ0 + IRQ_TIMER));
+  write(TIMER_INITIAL_COUNT, 1000000);
 
   // Disable logical interrupt lines.
-  write(LINT0, MASKED);
-  write(LINT1, MASKED);
+  write(LOCAL_INTERRUPT_0_PIN, MASKED);
+  write(LOCAL_INTERRUPT_1_PIN, MASKED);
 
-  // Disable performance counter overflow interrupts
-  // on machines that provide that interrupt entry.
-  // TODO: Fix offsets.
-  let version = LOCAL_INTERRUPT_CONTROLLER.offset(VERSION as isize).read_volatile();
+  // Disable performance counter overflow interrupts on machines that provide that interrupt entry.
+  let version = read(VERSION);
   if ((version >> 16) & 0xFF) >= 4 {
-    write(PCINT, MASKED);
+    write(PERFORMANCE_MONITORING_COUNTERS_LOCAL_VECTOR_TABLE, MASKED);
   }
 
   // Map error interrupt to IRQ_ERROR.
-  write(ERROR, T_IRQ0 + IRQ_ERROR);
+  write(ERROR_LOCAL_VECTOR_TABLE, T_IRQ0 + IRQ_ERROR);
 
   // Clear error status register (requires back-to-back writes).
-  write(ESR, 0);
-  write(ESR, 0);
+  write(ERROR_STATUS, 0);
+  write(ERROR_STATUS, 0);
 
   // Ack any outstanding interrupts.
-  write(EOI, 0);
+  write(END_OF_INTERRUPT, 0);
 
   // Send an Init Level De-Assert to synchronise arbitration ID's.
-  write(ICRHI, 0);
-  write(ICRLO, BCAST | INIT | LEVEL);
-  while LOCAL_INTERRUPT_CONTROLLER.offset(ICRLO as isize).read_volatile() & DELIVS != 0 {
+  write(INTERRUPT_COMMAND_HIGH, 0);
+  write(INTERRUPT_COMMAND_LOW, BROADCAST | INIT | LEVEL_TRIGGERED);
+  while read(INTERRUPT_COMMAND_LOW) & DELIVERY_STATUS != 0 {
   }
 
   // Enable interrupts on the APIC (but not on the processor).
-  write(TPR, 0);
+  write(TASK_PRIORITY, 0);
 
   println!("local interrupt controller enabled.");
 }
 
+/// Write the local interrupt controller register with the provided value and wait for the write to finish.
 unsafe fn write(register: u32, value: u32) {
-  (((LOCAL_INTERRUPT_CONTROLLER as usize).overflowing_add(register as usize).0) as *mut u32).write_volatile(value);
+  LOCAL_INTERRUPT_CONTROLLER.offset(register as isize).write_volatile(value);
   // Wait for write to finish, by reading
-  (((LOCAL_INTERRUPT_CONTROLLER as usize).overflowing_add(ID as usize).0) as *mut u32).read_volatile();
+  LOCAL_INTERRUPT_CONTROLLER.offset(ID as isize).read_volatile();
+}
+
+/// Read the local interrupt controller register with the provided value.
+unsafe fn read(register: u32) -> u32 {
+  LOCAL_INTERRUPT_CONTROLLER.offset(register as isize).read_volatile()
 }
